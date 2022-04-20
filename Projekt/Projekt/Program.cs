58d021456154c26ee81e81884571ce579683261b
nitrazek <99548252+nitrazek@users.noreply.github.com>
2022-04-20 09:42:10 +0200

    Inicjalizacja projektu
    Wygenerowanie projektu w c#

---
+++ Projekt/Projekt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projekt
+{
+    public class Projekt
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Tu będzie projekt");
+        }
+    }
+}
