06dac6e1b3b20f2bc21536830b9bef4199e6e12b
nitrazek <99548252+nitrazek@users.noreply.github.com>
2022-04-21 11:33:37 +0200

    Create Team.cs

---
+++ Projekt/Projekt/Team.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Projekt
+{
+    public class Team
+    {
+        private String name;
+        private int score;
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public int getScore()
+        {
+            return score;
+        }
+
+        public Team()
+        {
+            name = "[Brak nazwy]";
+            score = 0;
+        }
+
+        public Team(String name)
+        {
+            this.name = name;
+            this.score = 0;
+        }
+    }
+}
