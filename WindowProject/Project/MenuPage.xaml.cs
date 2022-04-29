d1a25f51eab103d779956a5f5240ae7f156e18d6
nitrazek <99548252+nitrazek@users.noreply.github.com>
2022-04-29 10:39:44 +0200

    Window Project

---
+++ WindowProject/Project/MenuPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace Project
+{
+    public partial class MenuPage : Page
+    {
+        private Window mainWindow;
+        private VolleyballPage volleyball;
+        private TugOfWarPage tugOfWar;
+        public MenuPage(MainWindow window)
+        {
+            InitializeComponent();
+            volleyball = new VolleyballPage(this);
+            tugOfWar = new TugOfWarPage(this);
+            mainWindow = window;
+        }
+        private void Volleyball_Button(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(volleyball);
+        }
+
+        private void TugOfWar_Button(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(tugOfWar);
+        }
+
+        private void Exit_Button(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Czy zapisać stan programu?", "Wyjście", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (result == MessageBoxResult.Yes)
+            {
+
+            }
+            mainWindow.Close();
+        }
+    }
+}
---
+++ WindowProject/Project/VolleyballPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace Project
+{
+    public partial class VolleyballPage : Page
+    {
+        private MenuPage _menu;
+        public VolleyballPage(MenuPage menu)
+        {
+            InitializeComponent();
+            _menu = menu;
+        }
+        private void GoBack_Button(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(_menu);
+        }
+    }
+}
