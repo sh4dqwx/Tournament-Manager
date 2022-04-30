f7260da533bd07ae05090d5db02ea7a5d2d7a556
xShyQx <bartosz.kar@wp.pl>
2022-04-30 23:25:50 +0200

    dodano podstrone do wczytywania z pliku

---
+++ WindowProject/Project/RegistrationPages/LoadPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project.RegistrationPages
+{
+    /// <summary>
+    /// Logika interakcji dla klasy LoadPage.xaml
+    /// </summary>
+    public partial class LoadPage : Page
+    {
+        private MenuPage _menu;
+        public LoadPage(MenuPage menu)
+        {
+            InitializeComponent();
+            _menu = menu;
+        }
+
+        private void Load_Button(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void GoBack_Button(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(_menu);
+        }
+    }
+}
