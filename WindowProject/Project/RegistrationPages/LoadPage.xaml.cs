0c32623e504fa875db45df26abeb8f2cdc090286
xShyQx <bartosz.kar@wp.pl>
2022-04-30 23:42:56 +0200

    Update LoadPage.xaml.cs

--- WindowProject/Project/RegistrationPages/LoadPage.xaml.cs
+++ WindowProject/Project/RegistrationPages/LoadPage.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
+using Project.Games;
+using Project.Registrations;
 
 namespace Project.RegistrationPages
 {
@@ -15,6 +18,52 @@ namespace Project.RegistrationPages
         {
             InitializeComponent();
             _menu = menu;
+        }
+
+        public static void load(string? fileName, Volleyball volleyball, Tug_of_war tugOfWar)
+        {
+            //Najpierw sprawdzamy czy nazwa pliku jest dobra i czy taki plik istnieje
+            if (fileName == null || fileName.Length == 0 || !File.Exists($@"..\..\..\saved\{fileName}.txt"))
+            {
+                Console.WriteLine("Niepoprawna nazwa pliku");
+                return;
+            }
+
+            //Usuwamy to co było wcześniej w listach
+            volleyball.clearTeams();
+            volleyball.clearJudges();
+            tugOfWar.clearTeams();
+            tugOfWar.clearJudges();
+
+            //Póki co tylko dla siatkówki, sprawdzamy czy dana linijka to drużyna czy sędzia, potem jaki sport, i dodajemy do odpowiedniej listy
+            StreamReader loadStream = new StreamReader($@"..\..\..\saved\{fileName}.txt");
+            while (!loadStream.EndOfStream)
+            {
+                string[] dane = loadStream.ReadLine().Split(',');
+                if (dane[0].Equals("T"))
+                {
+                    if (dane[1].Equals("V"))
+                    {
+                        volleyball.addTeam(new Team(dane[2]));
+                    }
+                    else if (dane[1].Equals("T"))
+                    {
+                        tugOfWar.addTeam(new Team(dane[2]));
+                    }
+                }
+                else if (dane[0].Equals("J"))
+                {
+                    if (dane[1].Equals("V"))
+                    {
+                        volleyball.addJudge(new Judge(dane[2], dane[3]));
+                    }
+                    else if (dane[1].Equals("T"))
+                    {
+                        tugOfWar.addJudge(new Judge(dane[2], dane[3]));
+                    }
+                }
+            }
+            loadStream.Close();
         }
 
         private void Load_Button(object sender, RoutedEventArgs e)
