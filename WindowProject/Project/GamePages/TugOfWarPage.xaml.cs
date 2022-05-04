e4095df5543007206123232373c6741a38ce5635
xShyQx <bartosz.kar@wp.pl>
2022-05-04 22:50:44 +0200

    przeciąganie liny działa

--- WindowProject/Project/GamePages/TugOfWarPage.xaml.cs
+++ WindowProject/Project/GamePages/TugOfWarPage.xaml.cs
@@ -2,6 +2,8 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
+using System.Collections.Generic;
+using Project.Registrations;
 
 namespace Project.GamePages
 {
@@ -13,9 +15,63 @@ namespace Project.GamePages
             InitializeComponent();
             _menu = menu;
         }
-        private void GoBack_Button(object sender, RoutedEventArgs e)
+        public void refreshPoints()
+        {
+            int lp = 1;
+            string place = "";
+            string names = "";
+            string points = "";
+            List<Team> teams = _menu.tugOfWar.getTeams();
+            teams.ForEach(team => { names += team.getName() + "\n"; points += team.getScore() + "\n"; place += lp + "\n"; lp += 1; });
+            teamPlace.Text = place;
+            teamName.Text = names;
+            teamPoints.Text = points;
+        }
+        private void GoBackButton(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(_menu);
+        }
+        private void ShowTeams_Button(object sender, RoutedEventArgs e)
+        {
+            if (_menu.tugOfWar.getTeams().Count <= 4)
+            {
+                MessageBox.Show("Za mało drużyn minimalna liczba to 5", "Uwaga", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            HEl.IsEnabled = true;
+            HRo.IsEnabled = false;
+            refreshPoints();
+        }
+        private void Elimination_Button(object sender, RoutedEventArgs e)
+        {
+            HCw.IsEnabled = true;
+            HEl.IsEnabled = false;
+
+            string score = _menu.tugOfWar.playElimination();
+            Content content = new Content(score);
+            content.Show();
+            refreshPoints();
+        }
+        private void Semi_Button(object sender, RoutedEventArgs e)
+        {
+            HFi.IsEnabled = true;
+            HCw.IsEnabled = false;
+
+            string score = _menu.tugOfWar.playSemiFinal();
+            Content content2 = new Content(score);
+            content2.Show();
+            refreshPoints();
+
+        }
+        private void Final_Button(object sender, RoutedEventArgs e)
+        {
+            HFi.IsEnabled = false;
+            HRo.IsEnabled = true;
+
+            string score = _menu.tugOfWar.playFinal();
+            Content content3 = new Content(score);
+            content3.Show();
+            refreshPoints();
         }
     }
 }
