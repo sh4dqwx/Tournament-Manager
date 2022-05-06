163847ee2d326fed140fd790c0552754eb5056ad
xShyQx <bartosz.kar@wp.pl>
2022-05-06 18:09:06 +0200

    dodałem folder na wyjątki z pierwszym do pustej nazwy

---
+++ WindowProject/Project/Exceptions/EmptyNameException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Exceptions
+{
+    class EmptyNameException : Exception
+    {
+        public EmptyNameException() { }
+        public EmptyNameException(string msg): base(msg) { }
+    }
+}
