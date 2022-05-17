8f7fadc614bc5dc3f61fe154132d3868cd26de46
xShyQx <bartosz.kar@wp.pl>
2022-05-17 17:21:52 +0200

    zaczynamy

--- WindowProject/Project/MenuPage.xaml.cs
+++ WindowProject/Project/MenuPage.xaml.cs
@@ -7,9 +7,11 @@ namespace Project
     public partial class MenuPage : Page
     {
         private Window _window;
+        private Program program;
         public MenuPage(MainWindow window)
         {
             InitializeComponent();
+            program = new Program();
             _window = window;
         }
     }
---
+++ WindowProject/Project/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Project
+{
+    public class Program
+    {
+        private List<string> tournaments;
+
+        public string[] getTournaments()
+        {
+            return tournaments.ToArray();
+        }
+        public void addTournament(string tournament)
+        {
+            tournaments.Add(tournament);
+        }
+        public void removeTournament(string tournament)
+        {
+            tournaments.Remove(tournament);
+        }
+        public void save(string fileName)
+        {
+            
+        }
+        public void load(string fileName)
+        {
+
+        }
+    }
+}
