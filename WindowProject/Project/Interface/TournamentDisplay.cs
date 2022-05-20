93ce0016de7a86d24ac584f569a3cf7903f56bf3
xShyQx <bartosz.kar@wp.pl>
2022-05-20 17:39:08 +0200

    tera wygląda

---
+++ WindowProject/Project/Interface/TournamentDisplay.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Interface
+{
+    public class TournamentDisplay
+    {
+        public TournamentDisplay(string name, int category)
+        {
+            Name = name;
+            switch (category)
+            {
+                case 0:
+                    Category = "Siatkówka";
+                    break;
+                case 1:
+                    Category = "Przeciąganie liny";
+                    break;
+                case 2:
+                    Category = "Dwa ognie";
+                    break;
+            }
+        }
+        public string Name { get; set; }
+        public string Category { get; set; }
+    }
+}
